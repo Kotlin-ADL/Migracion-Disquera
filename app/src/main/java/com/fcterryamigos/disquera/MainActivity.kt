c72fcfc96af92a20c11ca0ff005c4653daed968c
Dennis Mayr <dmayr.dev@gmail.com>
2025-06-25 15:38:41 -0400

    Corregido path del package para MainActivity.kt

--- app/src/main/java/com/fcterryamigos/disquera/MainActivity.kt
+++ app/src/main/java/com/fcterryamigos/disquera/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.fcterryamigos.disquera.vista
+package com.fcterryamigos.disquera
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.fcterryamigos.disquera.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
