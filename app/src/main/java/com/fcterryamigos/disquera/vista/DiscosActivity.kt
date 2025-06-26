a71c035770cac79940d1632ba88b993334fa5129
Felipe Parada V <felipeparadavasquez@gmail.com>
2025-06-25 21:36:32 -0400

    WIP: guardo cambios antes de mergear discos vista

--- app/src/main/java/com/fcterryamigos/disquera/vista/DiscosActivity.kt
+++ app/src/main/java/com/fcterryamigos/disquera/vista/DiscosActivity.kt
@@ -1,4 +1,15 @@
 package com.fcterryamigos.disquera.vista
 
-class DiscosActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.fcterryamigos.disquera.R
+
+class DiscosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_discos)
+
+    }
 }
