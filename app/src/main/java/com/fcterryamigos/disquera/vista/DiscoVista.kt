c3a68253e27928c24e749c043a1e33ea5eaae755
Dennis Mayr <dmayr.dev@gmail.com>
2025-06-26 12:37:21 -0400

    DiscoVista.kt: incorporados cambios de felipe1997

--- app/src/main/java/com/fcterryamigos/disquera/vista/DiscoVista.kt
+++ app/src/main/java/com/fcterryamigos/disquera/vista/DiscoVista.kt
@@ -1,5 +1,6 @@
 package com.fcterryamigos.disquera.vista
 
+import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,14 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.fcterryamigos.disquera.R
 
 class DiscoVista : AppCompatActivity() {
-    val ivAlbumCover = findViewById<ImageView>(R.id.ivAlbumCover)
-    val tvAlbumTitle = findViewById<TextView>(R.id.tvAlbumTitle)
-    val tvAlbumArtist = findViewById<TextView>(R.id.tvAlbumArtist)
 
-    val tvAnioData = findViewById<TextView>(R.id.tvAnioData)
-    val tvDiscograficaData = findViewById<TextView>(R.id.tvDiscograficaData)
-    val tvGeneroData = findViewById<TextView>(R.id.tvGeneroData)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    val tvPrecioData = findViewById<TextView>(R.id.tvPrecioData)
-    val btnCarrito = findViewById<Button>(R.id.btnCarrito)
+        val ivAlbumCover = findViewById<ImageView>(R.id.ivAlbumCover)
+        val tvAlbumTitle = findViewById<TextView>(R.id.tvAlbumTitle)
+        val tvAlbumArtist = findViewById<TextView>(R.id.tvAlbumArtist)
+
+        val tvAnioData = findViewById<TextView>(R.id.tvAnioData)
+        val tvDiscograficaData = findViewById<TextView>(R.id.tvDiscograficaData)
+        val tvGeneroData = findViewById<TextView>(R.id.tvGeneroData)
+
+        val tvPrecioData = findViewById<TextView>(R.id.tvPrecioData)
+        val btnCarrito = findViewById<Button>(R.id.btnCarrito)
+    }
 }
