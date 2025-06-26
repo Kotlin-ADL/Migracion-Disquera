86e4f48d83f71713ab257c996d44b5008d2b2cf6
maJoseVl <mjosevl28@gmail.com>
2025-06-26 17:39:29 -0400

    Commit de prueba: cambios en GenerosActivity y layout

--- app/src/main/java/com/fcterryamigos/disquera/vista/GenerosActivity.kt
+++ app/src/main/java/com/fcterryamigos/disquera/vista/GenerosActivity.kt
@@ -1,4 +1,32 @@
 package com.fcterryamigos.disquera.vista
 
-class GenerosActivity {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.fcterryamigos.disquera.R
+
+class GenerosActivity : AppCompatActivity() {
+
+    // Ejecuta y carga
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Carga el diseño de la pantalla desde el archivo activity_generos.xml
+        setContentView(R.layout.activity_generos)
+
+        // Buscamos el TextView por su ID definido en el XML
+        val titulo = findViewById<TextView>(R.id.tituloGeneros)
+
+        // Cambiamos el texto del título (opcional)
+        titulo.text = "Géneros musicales disponibles"
+
+        // Busca el botón por su ID definido en el XML
+        val btnVolver = findViewById<Button>(R.id.btnVolver)
+
+        // Cuando se hace clic en el botón, cerramos esta pantalla y volvemos atrás
+        btnVolver.setOnClickListener {
+            finish()
+        }
+    }
 }
